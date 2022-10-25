343c5ff7885d229df614126fd91b424f01ea5219
h2005 <h20051007@gmail.com>
2022-10-25 17:37:02 +0900

    [ADD]ロビー用のスコアボードをプレイヤーに設定する関数の追加 ※記述途中

--- src/main/java/com/github/maiotachannel/castlewars/player/scoreboard/LobbyScoreBoard.java
+++ src/main/java/com/github/maiotachannel/castlewars/player/scoreboard/LobbyScoreBoard.java
@@ -1,4 +1,30 @@
 package com.github.maiotachannel.castlewars.player.scoreboard;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+
 public class LobbyScoreBoard {
+
+    /**
+     * ロビー内用のスコアボードをプレイヤーに設定します
+     */
+
+    void setLobbyScoreBoard(){
+        ScoreboardManager scoreboardManager = Bukkit.getScoreboardManager();
+        Scoreboard scoreboard = scoreboardManager.getNewScoreboard();
+        Objective objective = scoreboard.registerNewObjective("LobbyScoreboard","dummy","CastleWars");
+        objective.getScore("＃日付").setScore(8);
+        objective.getScore("").setScore(7);
+        objective.getScore("Level: 数字").setScore(6);
+        objective.getScore("GameCoin: 数字").setScore(5);
+        objective.getScore(ChatColor.RESET.toString()).setScore(4);
+        objective.getScore("サーバー人数: 数字").setScore(3);
+        objective.getScore("Ping: 数字").setScore(2);
+        objective.getScore(ChatColor.RESET+ChatColor.RESET.toString()).setScore(1);
+        objective.getScore("サーバーIP").setScore(0);
+    }
+
 }
